7fd9b27b5f99b1a0a7dcc0bba90bec3cd8f8ea68
Sean Darcy <darcys22@gmail.com>
2014-01-01 14:05:06 -0800

    added some config options that were missing

--- lib/grandfather/config.rb
+++ lib/grandfather/config.rb
@@ -4,7 +4,7 @@ module Grandfather
 
   # Class for config params
   class Config
-    attr_accessor :output_filename, :file, :recursive, :wkhtmltopdf_parameters
+    attr_accessor :output_filename, :file, :recursive, :wkhtmltopdf_parameters, :stylesheet
 
     def initialize
       @output_filename = "Book.pdf"
